c802dbfc7ef25ef738dc11f3bf81c3ea562c04f8
Archishman Ghosh <ag2324@it.jgec.ac.in>
2021-08-03 10:54:25 +0530

    Added Round 736 A-D

---
+++ Codeforces/Round736/b.cpp
@@ -0,0 +1,89 @@
+// Created: 01-08-2021 20:12:47
+
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+const int mxN = 2e5 + 5;
+const int INF = 1e18 + 7;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+#ifndef ONLINE_JUDGE
+#define debugarr(x,i,j) cerr << #x <<" "; _print(x,i,j); cerr << endl;
+#else
+#define debugarr(x)
+#endif
+
+void _print(int t){cerr << t;}
+void _print(string t){cerr << t;}
+void _print(bool t){cerr << t;}
+void _print(char t){cerr << t;}
+void _print(long double t){cerr << t;}
+void _print(double t){cerr << t;}
+void _print(unsigned long long t){cerr << t;}
+template <class T> void _print(T arr[], int i, int j){cerr << "[ " ;for ( int k = i; k <= j; k++) cerr << arr[k] << ", " ;cerr << "]\n";}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p){cerr << "{";_print(p.ff);cerr << ",";_print(p.ss);cerr << "}";}
+template <class T> void _print(vector <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(set <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(multiset <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T, class V> void _print(map <T, V> v){cerr << "[ ";for (auto i : v){_print(i);cerr << " ";}cerr << "]";}
+
+int T, N;
+string S1, S2;
+
+void solve()
+{
+	cin >> N >> S1 >> S2;
+
+	int ans = 0;
+
+	for(int i = 0; i < N; i++)
+	{
+		if(S2[i] == '1')
+		{	
+			if(S1[i] == '0')
+			{
+				ans += 1;
+				S2[i] = '0';
+			}
+			else if((i - 1) >= 0 && S1[i - 1] == '1')
+			{
+				ans += 1;
+				S2[i] = '0';
+			}
+			else if((i + 1 < N) && (S1[i + 1] == '1'))
+			{
+				S1[i + 1] = '0';
+				S2[i] = '0';
+				ans += 1;
+			}
+		}
+	}
+
+	cout << ans << '\n';
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+        freopen("error.txt", "w", stderr);
+    #endif
+
+    cin >> T;
+    while (T--)
+        solve();
+
+    return 0;
+}
