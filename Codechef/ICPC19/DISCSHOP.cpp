7f32dc57e12bbdeeb2940ca9ece5e23c261b2ff5
Archishman Ghosh <ag2324@it.jgec.ac.in>
2021-08-07 12:55:00 +0530

    Added solutions to first 2 problems of prelims ICPC 2019

---
+++ Codechef/ICPC19/DISCSHOP.cpp
@@ -0,0 +1,91 @@
+// Created: 07-08-2021 10:22:22
+
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+const int mxN = 2e5 + 5;
+const int INF = 1e18 + 7;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+#ifndef ONLINE_JUDGE
+#define debugarr(x,i,j) cerr << #x <<" "; _print(x,i,j); cerr << endl;
+#else
+#define debugarr(x)
+#endif
+
+void _print(int t){cerr << t;}
+void _print(string t){cerr << t;}
+void _print(bool t){cerr << t;}
+void _print(char t){cerr << t;}
+void _print(long double t){cerr << t;}
+void _print(double t){cerr << t;}
+void _print(unsigned long long t){cerr << t;}
+template <class T> void _print(T arr[], int i, int j){cerr << "[ " ;for ( int k = i; k <= j; k++) cerr << arr[k] << ", " ;cerr << "]\n";}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p){cerr << "{";_print(p.ff);cerr << ",";_print(p.ss);cerr << "}";}
+template <class T> void _print(vector <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(set <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(multiset <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T, class V> void _print(map <T, V> v){cerr << "[ ";for (auto i : v){_print(i);cerr << " ";}cerr << "]";}
+
+int T;
+string S;
+
+int stringtointeger(string x)
+{
+    int ans = 0, j = 1;
+ 
+    for (int i = x.size() - 1; i >= 0; i--)
+    {
+        ans += (x[i] - '0') * j;
+        j *= 10;
+    }
+ 
+    return ans;
+}
+
+void solve()
+{
+	cin >> S;
+	int N = S.size();
+
+	int ans = stringtointeger(S);
+	for(int i = 0; i < N; i++)
+	{
+		string left = "", right = "";
+		if(i > 0)
+			left = S.substr(0, i);
+		if(i < N - 1)
+			right = S.substr(i + 1, (N - i));
+
+		string newS = left + right;
+		ans = min(ans, stringtointeger(newS));
+	}
+
+	cout << ans << '\n';
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+        freopen("error.txt", "w", stderr);
+    #endif
+
+    cin >> T;
+    while (T--)
+        solve();
+
+    return 0;
+}
