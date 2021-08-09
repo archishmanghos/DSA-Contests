cab370ad96926f91a9902aa0f891d4ecbf3bc72d
Archishman Ghosh <ag2324@it.jgec.ac.in>
2021-08-09 21:55:50 +0530

    Added solutions to CF round 737 A and B

---
+++ Codeforces/normal/737/b.cpp
@@ -0,0 +1,89 @@
+// Created: 09-08-2021 20:16:25
+
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+const int mxN = 2e5 + 5;
+const int INF = 1e18 + 7;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+#ifndef ONLINE_JUDGE
+#define debugarr(x,i,j) cerr << #x <<" "; _print(x,i,j); cerr << endl;
+#else
+#define debugarr(x)
+#endif
+
+void _print(int t){cerr << t;}
+void _print(string t){cerr << t;}
+void _print(bool t){cerr << t;}
+void _print(char t){cerr << t;}
+void _print(long double t){cerr << t;}
+void _print(double t){cerr << t;}
+void _print(unsigned long long t){cerr << t;}
+template <class T> void _print(T arr[], int i, int j){cerr << "[ " ;for ( int k = i; k <= j; k++) cerr << arr[k] << ", " ;cerr << "]\n";}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p){cerr << "{";_print(p.ff);cerr << ",";_print(p.ss);cerr << "}";}
+template <class T> void _print(vector <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(set <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(multiset <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T, class V> void _print(map <T, V> v){cerr << "[ ";for (auto i : v){_print(i);cerr << " ";}cerr << "]";}
+
+int T, N, K, A[mxN];
+
+void solve()
+{
+    cin >> N >> K;
+    int B[N + 5];
+
+    for(int i = 1; i <= N; i++)
+    {
+    	cin >> A[i];
+    	B[i] = A[i];
+    }
+
+    sort(B + 1, B + N + 1);
+    unordered_map<int, int> index;
+
+    for(int i = 1; i <= N; i++)
+    	index[B[i]] = i;
+
+    int increasing = 1;
+    for(int i = 2; i <= N; i++)
+    {
+    	if(A[i] > A[i - 1])
+    	{
+    		if(index[A[i]] - 1 == index[A[i - 1]])
+    			continue;
+    		else
+    			increasing += 1;
+    	}
+    	else
+    		increasing += 1;
+    }
+    cout << (K >= increasing ? "Yes" : "No") << '\n';
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+        freopen("error.txt", "w", stderr);
+    #endif
+
+    cin >> T;
+    while (T--)
+        solve();
+
+    return 0;
+}
