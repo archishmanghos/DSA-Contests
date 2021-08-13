64dab0eb94bd649a546abe5030a3231d8a1afe7e
Archishman Ghosh <ag2324@it.jgec.ac.in>
2021-08-13 11:50:52 +0530

    Removed useless code from Points on Line

--- Random/Codeforces/251A.cpp
+++ Random/Codeforces/251A.cpp
@@ -37,56 +37,6 @@ template <class T, class V> void _print(map <T, V> v){cerr << "[ ";for (auto i :
 
 int N, D, A[mxN];
 
-const int MOD = 1e9 + 7;
-int fact[mxN], invfact[mxN];
-
-int pow(int a, int b, int m)
-{
-	int ans = 1;
-	while(b)
-	{
-		if(b & 1)
-			ans = (ans *a) % m;
-		b /= 2;
-		a = (a *a) % m;
-	}
-	return ans;
-}
-
-int modinv(int k)
-{
-	return pow(k, MOD - 2, MOD);
-}
-
-void precompute()
-{
-	fact[0] = fact[1] = 1;
-	for(int i = 2; i < mxN; i++)
-	{
-		fact[i] = fact[i - 1] * i;
-		fact[i] %= MOD;
-	}
-	invfact[mxN - 1] = modinv(fact[mxN - 1]);
-	
-	for(int i = mxN - 2; i >= 0; i--)
-	{
-		invfact[i] = invfact[i + 1] * (i + 1);
-		invfact[i] %= MOD;
-	}
-}
-
-int nCr(int x, int y)
-{
-	if(y > x)
-		return 0;
-	int num = fact[x];
-	num *= invfact[y];
-	num %= MOD;
-	num *= invfact[x - y];
-	num %= MOD;
-	return num;
-}
-
 int32_t main()
 {
     ios_base::sync_with_stdio(false);
@@ -95,8 +45,6 @@ int32_t main()
 	#ifndef ONLINE_JUDGE
 	    freopen("error.txt", "w", stderr);
 	#endif
-
-	precompute();
 
 	cin >> N >> D;
 	for(int i = 1; i <= N; i++)
@@ -114,17 +62,13 @@ int32_t main()
 		if(abs(A[j] - A[i]) <= D)
 		{
 			if(j - i + 1 >= 3)
-			{
 				ans += ((j - i) * (j - i - 1)) / 2;
-			}
 			j += 1;
 		}
 		else
-		{
-			
 			i += 1;
-		}
 	}
+
 	cout << ans;
 
     return 0;
