441f60d4c54c61e4405f169cbe8d37d2ef62f113
Archishman Ghosh <ag2324@it.jgec.ac.in>
2021-08-16 21:58:48 +0530

    Added solutions to Phoenix and Balance, Array Cancellation, Remainder,  Belted Rooms and Perform the Combo

---
+++ Random/Codeforces/1165A.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+const int mxN = 2e5 + 5;
+const int INF = 1e18 + 7;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+#ifndef ONLINE_JUDGE
+#define debugarr(x,i,j) cerr << #x <<" "; _print(x,i,j); cerr << endl;
+#else
+#define debugarr(x)
+#endif
+
+void _print(int t){cerr << t;}
+void _print(string t){cerr << t;}
+void _print(bool t){cerr << t;}
+void _print(char t){cerr << t;}
+void _print(long double t){cerr << t;}
+void _print(double t){cerr << t;}
+void _print(unsigned long long t){cerr << t;}
+template <class T> void _print(T arr[], int i, int j){cerr << "[ " ;for ( int k = i; k <= j; k++) cerr << arr[k] << ", " ;cerr << "]\n";}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p){cerr << "{";_print(p.ff);cerr << ",";_print(p.ss);cerr << "}";}
+template <class T> void _print(vector <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(set <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(multiset <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T, class V> void _print(map <T, V> v){cerr << "[ ";for (auto i : v){_print(i);cerr << " ";}cerr << "]";}
+
+int N, X, Y;
+string S;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	#ifndef ONLINE_JUDGE
+	    freopen("error.txt", "w", stderr);
+	#endif
+
+	cin >> N >> X >> Y >> S;
+
+	string remainder = "1", divisor = "1";
+	for(int i = 1; i <= Y; i++)
+		remainder += '0';
+	for(int i = 1; i <= X; i++)
+		divisor += '0';
+
+	int ans = 0;
+	for(int i = N - 1, j = Y; i >= (N - Y - 1); i--, j--)
+	{
+		ans += S[i] != remainder[j];
+		debug(i)
+		debug(j)
+	}
+	debug(ans)
+	debug(divisor)
+	for(int i = (N - Y - 2), j = X; i >= (N - X); i--, j--)
+	{
+		ans += S[i] != '0';
+		debug(i)
+		debug(j)
+	}
+
+	cout << ans << '\n';
+	
+    return 0;
+}
