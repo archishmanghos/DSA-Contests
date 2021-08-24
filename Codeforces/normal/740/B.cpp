9aa2d402928d0b09f2fd364ab272bf640e958fc4
Archishman Ghosh <ag2324@it.jgec.ac.in>
2021-08-24 23:47:12 +0530

    Added solution to Round 740 A, B and C

---
+++ Codeforces/normal/740/B.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+const int mxN = 2e5 + 5;
+const int INF = 1e18 + 7;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+#ifndef ONLINE_JUDGE
+#define debugarr(x,i,j) cerr << #x <<" "; _print(x,i,j); cerr << endl;
+#else
+#define debugarr(x)
+#endif
+
+void _print(int t){cerr << t;}
+void _print(string t){cerr << t;}
+void _print(bool t){cerr << t;}
+void _print(char t){cerr << t;}
+void _print(long double t){cerr << t;}
+void _print(double t){cerr << t;}
+void _print(unsigned long long t){cerr << t;}
+template <class T> void _print(T arr[], int i, int j){cerr << "[ " ;for ( int k = i; k <= j; k++) cerr << arr[k] << ", " ;cerr << "]\n";}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p){cerr << "{";_print(p.ff);cerr << ",";_print(p.ss);cerr << "}";}
+template <class T> void _print(vector <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(set <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(multiset <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T, class V> void _print(map <T, V> v){cerr << "[ ";for (auto i : v){_print(i);cerr << " ";}cerr << "]";}
+
+int T, A, B;
+
+void solve()
+{
+    cin >> A >> B;
+    set<int> ans;
+
+    if((A + B) & 1)
+    {
+    	int minn = min(A, B);
+    	int start = (A + B) / 2;
+
+    	for(int i = start; i >= (start - minn); i--)
+    		ans.insert(i);
+    	for(int i = start + 1; i <= (start + 1 + minn); i++)
+    		ans.insert(i);
+    }
+    else
+    {
+    	if((A % 2 == 0) && (B % 2 == 0))
+    	{
+    		int minn = min(A, B);
+    		int k = (A + B) / 2;
+    		for(int j = 0; j <= minn; j += 2)
+    		{
+    			ans.insert(k);
+    			ans.insert(A + B - k);
+    			k += 2;
+    		}
+    	}
+    	else
+    	{
+    		int minn = min(A, B);
+    		int k = (A + B) / 2;
+    		k += 1;
+    		for(int j = 1; j <= minn; j += 2)
+    		{
+    			ans.insert(k);
+    			ans.insert(A + B - k);
+    			k += 2;
+    		}
+    	}
+    }
+
+    cout << ans.size() << '\n';
+    for(auto it : ans)
+    	cout << it << ' ';
+    cout << '\n';
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+        freopen("error.txt", "w", stderr);
+    #endif
+
+    cin >> T;
+    while (T--)
+        solve();
+
+    return 0;
+}
---
+++ Codeforces/normal/740/C.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+const int mxN = 2e5 + 5;
+const int INF = 1e18 + 7;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+#ifndef ONLINE_JUDGE
+#define debugarr(x,i,j) cerr << #x <<" "; _print(x,i,j); cerr << endl;
+#else
+#define debugarr(x)
+#endif
+
+void _print(int t){cerr << t;}
+void _print(string t){cerr << t;}
+void _print(bool t){cerr << t;}
+void _print(char t){cerr << t;}
+void _print(long double t){cerr << t;}
+void _print(double t){cerr << t;}
+void _print(unsigned long long t){cerr << t;}
+template <class T> void _print(T arr[], int i, int j){cerr << "[ " ;for ( int k = i; k <= j; k++) cerr << arr[k] << ", " ;cerr << "]\n";}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p){cerr << "{";_print(p.ff);cerr << ",";_print(p.ss);cerr << "}";}
+template <class T> void _print(vector <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(set <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(multiset <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T, class V> void _print(map <T, V> v){cerr << "[ ";for (auto i : v){_print(i);cerr << " ";}cerr << "]";}
+
+int T, N, K;
+
+void solve()
+{
+    cin >> N;
+
+    vector<pair<int,vector<int>>> A;
+    for(int i = 1; i <= N; i++)
+    {
+    	cin >> K;
+    	vector<int> temp;
+ 		int x;
+    	int maxx = 0;
+    	for(int j = 1; j <= K; j++)
+    	{
+    		cin >> x;
+    		maxx = max(maxx, ((x + 1) - (j - 1)));
+    		temp.push_back(x);
+    	}
+    	A.push_back({maxx, temp});
+    }
+
+    sort(A.begin(), A.end());
+
+    int sum = 0, ans = 0, monstersKilled = 0;
+    for(int i = 0; i < A.size(); i++)
+    {
+    	debug(A[i].first)
+    	for(int j = 0; j < A[i].second.size(); j++)
+    	{
+    		if(sum <= A[i].second[j])
+    		{
+    			ans = A[i].second[j] + 1 - monstersKilled;
+    			debug(ans)
+    			sum = A[i].second[j] + 1;
+    		}
+    		sum += 1;
+    		monstersKilled += 1;
+    	}
+    }
+
+    cout << ans << '\n';
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+        freopen("error.txt", "w", stderr);
+    #endif
+
+    cin >> T;
+    while (T--)
+        solve();
+
+    return 0;
+}
