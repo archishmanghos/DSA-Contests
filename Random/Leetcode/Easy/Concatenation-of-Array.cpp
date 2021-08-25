100731f2fd49cefbc7d89baa8a3203c247e76421
Archishman Ghosh <ag2324@it.jgec.ac.in>
2021-08-25 18:19:46 +0530

    Added solution to Concatenation of Array

---
+++ Random/Leetcode/Easy/Concatenation-of-Array.cpp
@@ -0,0 +1,18 @@
+vector<int> getConcatenation(vector<int>& nums) 
+{
+    int N=nums.size();
+    for(int i=0; i<N; i++)
+        nums.push_back(nums[i]);
+    return nums;
+}
+
+/*
+
+Analysis:
+
+Time Complexity: O(nums.size())
+Space Complexity: O(1)
+
+Easy, just push to the original vector instead of a new one to avoid more space.
+
+*/
