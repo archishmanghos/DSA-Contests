13471f54992c0def28d8136f90dacf90bc4b9c86
Archishman Ghosh <ag2324@it.jgec.ac.in>
2021-08-29 22:27:07 +0530

    Added solutions to ABC 216 A, B, C, D, E

---
+++ Atcoder/ABC/216/E.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+const int mxN = 2e5 + 5;
+const int INF = 1e18 + 7;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+#ifndef ONLINE_JUDGE
+#define debugarr(x,i,j) cerr << #x <<" "; _print(x,i,j); cerr << endl;
+#else
+#define debugarr(x)
+#endif
+
+void _print(int t){cerr << t;}
+void _print(string t){cerr << t;}
+void _print(bool t){cerr << t;}
+void _print(char t){cerr << t;}
+void _print(long double t){cerr << t;}
+void _print(double t){cerr << t;}
+void _print(unsigned long long t){cerr << t;}
+template <class T> void _print(T arr[], int i, int j){cerr << "[ " ;for ( int k = i; k <= j; k++) cerr << arr[k] << ", " ;cerr << "]\n";}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p){cerr << "{";_print(p.ff);cerr << ",";_print(p.ss);cerr << "}";}
+template <class T> void _print(vector <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(set <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(multiset <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T, class V> void _print(map <T, V> v){cerr << "[ ";for (auto i : v){_print(i);cerr << " ";}cerr << "]";}
+
+int N, K, A[mxN];
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	#ifndef ONLINE_JUDGE
+	    freopen("error.txt", "w", stderr);
+	#endif
+
+	cin >> N >> K;
+	for(int i = 1; i <= N; i++)
+		cin >> A[i];
+
+	sort(A + 1, A + N + 1, greater<int>());
+
+	int ans = 0;
+	for(int i = 1; i <= N; i++)
+	{
+		if((A[i] - A[i + 1])*i <= K)
+		{
+			int rem = A[i] - A[i + 1];
+			int calculateFromStart = A[i] * (A[i] + 1) / 2;
+			int calculateFromMiddle = A[i + 1] * (A[i + 1] + 1) / 2;
+			ans += i * (calculateFromStart - calculateFromMiddle);
+			K -= rem * i;
+		}
+		else
+		{
+			int rounds = K / i;
+			debug(rounds)
+			int calculateFromStart = A[i];
+			int calculateFromMiddle = A[i] - rounds;
+
+			calculateFromStart = calculateFromStart * (calculateFromStart + 1) / 2;
+			calculateFromMiddle = calculateFromMiddle * (calculateFromMiddle + 1) / 2;
+
+			ans += i * (calculateFromStart - calculateFromMiddle);
+			debug(ans)
+			ans += (A[i] - rounds) * (K % i);
+			debug(ans)
+			K = 0;
+			break;
+		}
+		debug(ans)
+		debug(K)
+	}
+
+	cout << ans;
+
+    return 0;
+}
