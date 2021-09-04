85cf535e3f2f17a401f2dbc8d58f64d2bae160b7
Archishman Ghosh <ag2324@it.jgec.ac.in>
2021-09-04 19:45:34 +0530

    Added solutions to ABC 217 A, B, C, D

---
+++ Atcoder/ABC/217/B.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+const int mxN = 2e5 + 5;
+const int INF = 1e18 + 7;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+#ifndef ONLINE_JUDGE
+#define debugarr(x,i,j) cerr << #x <<" "; _print(x,i,j); cerr << endl;
+#else
+#define debugarr(x)
+#endif
+
+void _print(int t){cerr << t;}
+void _print(string t){cerr << t;}
+void _print(bool t){cerr << t;}
+void _print(char t){cerr << t;}
+void _print(long double t){cerr << t;}
+void _print(double t){cerr << t;}
+void _print(unsigned long long t){cerr << t;}
+template <class T> void _print(T arr[], int i, int j){cerr << "[ " ;for ( int k = i; k <= j; k++) cerr << arr[k] << ", " ;cerr << "]\n";}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p){cerr << "{";_print(p.ff);cerr << ",";_print(p.ss);cerr << "}";}
+template <class T> void _print(vector <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(set <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(multiset <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T, class V> void _print(map <T, V> v){cerr << "[ ";for (auto i : v){_print(i);cerr << " ";}cerr << "]";}
+
+string S[3];
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	#ifndef ONLINE_JUDGE
+	    freopen("error.txt", "w", stderr);
+	#endif
+
+	string arr[4] = {"ABC", "ARC", "AGC", "AHC"};
+	sort(arr, arr + 4);
+
+	cin >> S[0] >> S[1] >> S[2];
+	sort(S, S + 3);
+
+	for(int i = 0; i < 3; i++)
+	{
+		if(arr[i] != S[i])
+		{
+			cout << arr[i];
+			return 0;
+		}
+	}
+
+	cout << arr[3];
+
+    return 0;
+}
