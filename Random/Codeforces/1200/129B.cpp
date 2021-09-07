3ecc145f378ae7e619a25b76f6402ea17e37be74
Archishman Ghosh <ag2324@it.jgec.ac.in>
2021-09-07 18:27:42 +0530

    Added solution to 129B Students and Shoelaces

---
+++ Random/Codeforces/1200/129B.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+const int mxN = 2e5 + 5;
+const int INF = 1e18 + 7;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+#ifndef ONLINE_JUDGE
+#define debugarr(x,i,j) cerr << #x <<" "; _print(x,i,j); cerr << endl;
+#else
+#define debugarr(x)
+#endif
+
+void _print(int t){cerr << t;}
+void _print(string t){cerr << t;}
+void _print(bool t){cerr << t;}
+void _print(char t){cerr << t;}
+void _print(long double t){cerr << t;}
+void _print(double t){cerr << t;}
+void _print(unsigned long long t){cerr << t;}
+template <class T> void _print(T arr[], int i, int j){cerr << "[ " ;for ( int k = i; k <= j; k++) cerr << arr[k] << ", " ;cerr << "]\n";}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p){cerr << "{";_print(p.ff);cerr << ",";_print(p.ss);cerr << "}";}
+template <class T> void _print(vector <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(set <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(multiset <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T, class V> void _print(map <T, V> v){cerr << "[ ";for (auto i : v){_print(i);cerr << " ";}cerr << "]";}
+
+int N, M, U, V;
+bool visited[200];
+set<int> adj[200];
+
+bool checkIfDone()
+{
+	bool ans = true;
+	for(int i = 1; i <= N; i++)
+		ans &= (adj[i].size() == 0);
+
+	return ans;
+}
+
+bool checkIfValidLoop()
+{
+	bool ans = false;
+	for(int i = 1; i <= N; i++)
+		ans |= (adj[i].size() == 1);
+
+	return ans;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	#ifndef ONLINE_JUDGE
+	    freopen("error.txt", "w", stderr);
+	#endif
+
+	cin >> N >> M;
+	for(int i = 1; i <= M; i++)
+	{
+		cin >> U >> V;
+		adj[U].insert(V);
+		adj[V].insert(U);
+	}
+
+	int ans = 0;
+	while(1)
+	{
+		if(checkIfDone())
+		{
+			cout << ans;
+			return 0;
+		}
+
+		if(checkIfValidLoop())
+		{
+			vector<int> eraseThem;
+			for(int i = 1; i <= N; i++)
+				if(adj[i].size() == 1)
+					eraseThem.push_back(i);
+
+			for(auto it : eraseThem)
+			{
+				int rem = *adj[it].begin();
+				adj[it].erase(rem);
+				adj[rem].erase(it);
+			}
+		}
+		else
+		{
+			cout << ans;
+			return 0;
+		}
+
+		ans += 1;
+	}
+
+    return 0;
+}
