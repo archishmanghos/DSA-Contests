cfa0c6177b3b4f8ed3a6c7ac66db3e7b2ebd8112
Archishman Ghosh <ag2324@it.jgec.ac.in>
2021-09-08 17:10:00 +0530

    Added solution to 469B Chat Online

---
+++ Random/Codeforces/1300/469B.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+const int mxN = 2e5 + 5;
+const int INF = 1e18 + 7;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+#ifndef ONLINE_JUDGE
+#define debugarr(x,i,j) cerr << #x <<" "; _print(x,i,j); cerr << endl;
+#else
+#define debugarr(x)
+#endif
+
+void _print(int t){cerr << t;}
+void _print(string t){cerr << t;}
+void _print(bool t){cerr << t;}
+void _print(char t){cerr << t;}
+void _print(long double t){cerr << t;}
+void _print(double t){cerr << t;}
+void _print(unsigned long long t){cerr << t;}
+template <class T> void _print(T arr[], int i, int j){cerr << "[ " ;for ( int k = i; k <= j; k++) cerr << arr[k] << ", " ;cerr << "]\n";}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p){cerr << "{";_print(p.ff);cerr << ",";_print(p.ss);cerr << "}";}
+template <class T> void _print(vector <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(set <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(multiset <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T, class V> void _print(map <T, V> v){cerr << "[ ";for (auto i : v){_print(i);cerr << " ";}cerr << "]";}
+
+struct point
+{
+	int x, y;
+};
+
+int P, Q;
+point interval, A[100], B[100];
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	#ifndef ONLINE_JUDGE
+	    freopen("error.txt", "w", stderr);
+	#endif
+
+	cin >> P >> Q >> interval.x >> interval.y;
+	for(int i = 1; i <= P; i++)
+		cin >> A[i].x >> A[i].y;
+	for(int i = 1; i <= Q; i++)
+		cin >> B[i].x >> B[i].y;
+
+	int ans = 0;
+	for(int i = interval.x; i <= interval.y; i++)
+	{
+		bool match = false;
+		for(int j = 1; j <= Q; j++)
+		{
+			point check;
+			check.x = B[j].x + i;
+			check.y = B[j].y + i;
+			
+			for(int k = 1; k <= P; k++)
+			{
+				if(check.y < A[k].x || check.x > A[k].y)
+					continue;
+				match = true;
+				if(match)
+					break;
+			}
+			
+			if(match)
+				break;
+		}
+
+		ans += match == true;
+	}
+
+	cout << ans;
+
+    return 0;
+}
