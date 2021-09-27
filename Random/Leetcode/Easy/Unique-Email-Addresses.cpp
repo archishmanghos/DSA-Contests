25e9436ca4a8e8b005012530f40087a3ac388499
Archishman Ghosh <ag2324@it.jgec.ac.in>
2021-09-27 22:20:19 +0530

    Add solution to 929 Unique Email Addresses

---
+++ Random/Leetcode/Easy/Unique-Email-Addresses.cpp
@@ -0,0 +1,36 @@
+string convertToActualEmail(string s)
+{
+    int pos;
+    for(int i = 0; i < s.size(); i++)
+    {
+        if(s[i] == '@')
+        {
+            pos = i;
+            break;
+        }
+    }
+    string firstPart = s.substr(0, pos);
+    string ans = "";
+    for(int i = 0; i < firstPart.size(); i++)
+    {
+        if(firstPart[i] == '.')
+            continue;
+        if(firstPart[i] == '+')
+            break;
+        ans += firstPart[i];
+    }
+    for(int i = pos; i < s.size(); i++)
+        ans += s[i];
+    return ans;
+}
+int numUniqueEmails(vector<string> &emails)
+{
+    set<string> ans;
+    for(int i = 0; i < emails.size(); i++)
+    {
+        string goodEmail = convertToActualEmail(emails[i]);
+        ans.insert(goodEmail);
+    }
+    int x = (int)ans.size();
+    return x;
+}
