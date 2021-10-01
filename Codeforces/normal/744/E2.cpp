79dc428ba0025ac29a7d18bde34ce371e4bfdcac
Archishman Ghosh <ag2324@it.jgec.ac.in>
2021-10-01 13:04:02 +0530

    Add solution to E2

---
+++ Codeforces/normal/744/E2.cpp
@@ -0,0 +1,78 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+using namespace __gnu_pbds;
+const int mxN = 2e5 + 5;
+const int INF = 1e18 + 7;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+#ifndef ONLINE_JUDGE
+#define debugarr(x,i,j) cerr << #x <<" "; _print(x,i,j); cerr << endl;
+#else
+#define debugarr(x)
+#endif
+
+void _print(int t){cerr << t;}
+void _print(string t){cerr << t;}
+void _print(bool t){cerr << t;}
+void _print(char t){cerr << t;}
+void _print(long double t){cerr << t;}
+void _print(double t){cerr << t;}
+void _print(unsigned long long t){cerr << t;}
+template <class T> void _print(T arr[], int i, int j){cerr << "[ " ;for ( int k = i; k <= j; k++) cerr << arr[k] << ", " ;cerr << "]\n";}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p){cerr << "{";_print(p.ff);cerr << ",";_print(p.ss);cerr << "}";}
+template <class T> void _print(vector <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(set <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(multiset <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T, class V> void _print(map <T, V> v){cerr << "[ ";for (auto i : v){_print(i);cerr << " ";}cerr << "]";}
+template <typename num_t>
+using ordered_set = tree<num_t, null_type, less_equal<num_t>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int T, N, A;
+
+void solve()
+{
+    cin >> N;
+    int ans = 0;
+    ordered_set<int> os_inc, os_dec;
+
+    for(int i = 1; i <= N; i++)
+    {
+    	cin >> A;
+    	int left = os_inc.order_of_key(A);
+    	int right = os_dec.order_of_key(-A);
+    	ans += min(left, right);
+    	os_inc.insert(A);
+    	os_dec.insert(-A);
+    }
+
+   	cout << ans << '\n';
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+        freopen("error.txt", "w", stderr);
+    #endif
+
+    cin >> T;
+    while (T--)
+        solve();
+
+    return 0;
+}
