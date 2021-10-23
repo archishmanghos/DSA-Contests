71bb116493bff98a8b21b97ee3d96e4122c8cafc
Archishman Ghosh <ag2324@it.jgec.ac.in>
2021-10-23 20:34:39 +0530

    Add solution to A, B, C of Atcoder 224

---
+++ Atcoder/ABC/224/B.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+const int mxN = 2e5 + 5;
+const int INF = 1e18 + 7;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+#ifndef ONLINE_JUDGE
+#define debugarr(x,i,j) cerr << #x <<" "; _print(x,i,j); cerr << endl;
+#else
+#define debugarr(x)
+#endif
+
+void _print(int t){cerr << t;}
+void _print(string t){cerr << t;}
+void _print(bool t){cerr << t;}
+void _print(char t){cerr << t;}
+void _print(long double t){cerr << t;}
+void _print(double t){cerr << t;}
+void _print(unsigned long long t){cerr << t;}
+template <class T> void _print(T arr[], int i, int j){cerr << "[ " ;for ( int k = i; k <= j; k++) cerr << arr[k] << ", " ;cerr << "]\n";}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p){cerr << "{";_print(p.ff);cerr << ",";_print(p.ss);cerr << "}";}
+template <class T> void _print(vector <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(set <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(multiset <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T, class V> void _print(map <T, V> v){cerr << "[ ";for (auto i : v){_print(i);cerr << " ";}cerr << "]";}
+
+int H, W, A[55][55];
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	#ifndef ONLINE_JUDGE
+	    freopen("error.txt", "w", stderr);
+	#endif
+
+	cin >> H >> W;
+	for(int i = 1; i <= H; i++)
+		for(int j = 1; j <= W; j++)
+			cin >> A[i][j];
+
+	bool ans = true;
+	for(int i = 1; i < H; i++)
+	{
+		for(int j = i + 1; j <= H; j++)
+		{
+			int x1 = i, x2 = j;
+			for(int k = 1; k < W; k++)
+			{
+				for(int l = k + 1; l <= W; l++)
+				{
+					int y1 = k, y2 = l;
+					ans &= (A[x1][y1] + A[x2][y2] <= A[x2][y1] + A[x1][y2]);
+				}
+			}
+		}
+	}
+
+	cout << (ans ? "Yes" : "No");
+
+    return 0;
+}
