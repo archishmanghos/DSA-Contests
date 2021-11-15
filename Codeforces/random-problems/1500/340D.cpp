7f1b1695d4f0bd04e3c6ff7dbae382a3619e8a12
Archishman Ghosh <ag2324@it.jgec.ac.in>
2021-11-15 17:04:25 +0530

    Add solution to 340D Bubble Sort Graph

---
+++ Codeforces/random-problems/1500/340D.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+const int mxN = 2e5 + 5;
+const int INF = 1e18 + 7;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+#ifndef ONLINE_JUDGE
+#define debugarr(x,i,j) cerr << #x <<" "; _print(x,i,j); cerr << endl;
+#else
+#define debugarr(x)
+#endif
+
+void _print(int t){cerr << t;}
+void _print(string t){cerr << t;}
+void _print(bool t){cerr << t;}
+void _print(char t){cerr << t;}
+void _print(long double t){cerr << t;}
+void _print(double t){cerr << t;}
+void _print(unsigned long long t){cerr << t;}
+template <class T> void _print(T arr[], int i, int j){cerr << "[ " ;for ( int k = i; k <= j; k++) cerr << arr[k] << ", " ;cerr << "]\n";}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p){cerr << "{";_print(p.first);cerr << ",";_print(p.second);cerr << "}";}
+template <class T> void _print(vector <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(set <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(multiset <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T, class V> void _print(map <T, V> v){cerr << "[ ";for (auto i : v){_print(i);cerr << " ";}cerr << "]";}
+
+int N;
+vector<int> A(mxN);
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	#ifndef ONLINE_JUDGE
+	    freopen("error.txt", "w", stderr);
+	#endif
+
+	cin >> N;
+	for(int i = 1; i <= N; i++)
+		cin >> A[i];
+
+	vector<int> lisLength;
+	for(int i = 1; i <= N; i++)
+	{
+		if(lisLength.size() == 0 || A[i] > lisLength.back())
+			lisLength.push_back(A[i]);
+		else
+		{
+			int j = 0, k = lisLength.size() - 1, pos = k;
+			while(j < k)
+			{
+				int mid = j + (k - j) / 2;
+				if(lisLength[mid] < A[i])
+					j = mid + 1;
+				else if(lisLength[mid] == A[i])
+				{
+					pos = min(pos, mid);
+					break;
+				}
+				else
+				{
+					pos = min(pos, mid);
+					k = mid;
+				}
+			}
+			lisLength[pos] = A[i];
+		}
+	}
+
+	cout << (int)lisLength.size();
+
+    return 0;
+}
