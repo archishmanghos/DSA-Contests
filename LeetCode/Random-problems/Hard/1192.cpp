edbf3b97f3dd7bbb3cf3c720bb0947c0c6c9d969
Archishman Ghosh <ag2324@it.jgec.ac.in>
2021-11-16 23:35:03 +0530

    Add solution to 1192 Critical Connections in a Network

---
+++ LeetCode/Random-problems/Hard/1192.cpp
@@ -0,0 +1,43 @@
+void dfs(int node, int parent, vector<bool> &visited, vector<int> &tin, vector<int> &low, int &timer, vector<vector<int>> &adj, vector<vector<int>> &ans)
+{
+    tin[node] = low[node] = timer;
+    timer += 1;
+    visited[node] = true;
+
+    for(int child : adj[node])
+    {
+        if(child == parent)
+            continue;
+        if(visited[child])
+            low[node] = min(low[node], low[child]);
+        else
+        {
+            dfs(child, node, visited, tin, low, timer, adj, ans);
+            low[node] = min(low[node], low[child]);
+
+            if(low[child] > tin[node])
+            {
+                vector<int> temp = {node, child};
+                ans.push_back(temp);
+            }
+        }
+    }
+}
+
+vector<vector<int>> criticalConnections(int n, vector<vector<int>> &connections)
+{
+    vector<vector<int>> adj(n);
+    for(int i = 0; i < connections.size(); i++)
+    {
+        adj[connections[i][0]].push_back(connections[i][1]);
+        adj[connections[i][1]].push_back(connections[i][0]);
+    }
+
+    vector<vector<int>> ans;
+    vector<bool> visited(n + 2, false);
+    vector<int> tin(n + 2), low(n + 2);
+    int timer = 1;
+    dfs(0, -1, visited, tin, low, timer, adj, ans);
+
+    return ans;
+}
