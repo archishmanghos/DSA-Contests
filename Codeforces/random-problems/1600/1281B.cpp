cec8f4b7a7a2b992f1f5f3484707b4eafe0e2197
Archishman Ghosh <ag2324@it.jgec.ac.in>
2021-11-21 11:59:53 +0530

    Add solution to 1281B Azamon Web Services

---
+++ Codeforces/random-problems/1600/1281B.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+const int mxN = 2e5 + 5;
+const int INF = 1e18 + 7;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+#ifndef ONLINE_JUDGE
+#define debugarr(x,i,j) cerr << #x <<" "; _print(x,i,j); cerr << endl;
+#else
+#define debugarr(x)
+#endif
+
+void _print(int t){cerr << t;}
+void _print(string t){cerr << t;}
+void _print(bool t){cerr << t;}
+void _print(char t){cerr << t;}
+void _print(long double t){cerr << t;}
+void _print(double t){cerr << t;}
+void _print(unsigned long long t){cerr << t;}
+template <class T> void _print(T arr[], int i, int j){cerr << "[ " ;for ( int k = i; k <= j; k++) cerr << arr[k] << ", " ;cerr << "]\n";}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p){cerr << "{";_print(p.first);cerr << ",";_print(p.second);cerr << "}";}
+template <class T> void _print(vector <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(set <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(multiset <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T, class V> void _print(map <T, V> v){cerr << "[ ";for (auto i : v){_print(i);cerr << " ";}cerr << "]";}
+
+int T;
+string S1, S2;
+
+void solve()
+{
+    cin >> S1 >> S2;
+    int N1 = S1.size(), N2 = S2.size();
+
+    string T = S1;
+    sort(T.begin(), T.end());
+
+    if(T >= S2)
+    {
+        cout << "---" << '\n';
+        return;
+    }
+
+    for(int i = 0; i < N1; i++)
+    {
+        if(S1[i] == T[i])
+            continue;
+        for(int j = i + 1; j < N1; j++)
+        {
+            if(S1[j] == T[i])
+            {
+                swap(S1[j], S1[i]);
+                if(S1 < S2)
+                {
+                    cout << S1 << '\n';
+                    return;
+                }
+                swap(S1[j], S1[i]);
+            }
+        }
+    }
+
+    if(S1 < S2)
+        cout << S1 << '\n';
+    else
+        cout << "---" << '\n';
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+        freopen("error.txt", "w", stderr);
+    #endif
+
+    cin >> T;
+    while (T--)
+        solve();
+
+    return 0;
+}
