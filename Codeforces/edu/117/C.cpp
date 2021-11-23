5ef80f986068539abb73fd3f3e7350b103811ed9
Archishman Ghosh <ag2324@it.jgec.ac.in>
2021-11-23 10:46:38 +0530

    Add solution to Edu C

---
+++ Codeforces/edu/117/C.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+const int mxN = 2e5 + 5;
+const int INF = 1e18 + 7;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+#ifndef ONLINE_JUDGE
+#define debugarr(x,i,j) cerr << #x <<" "; _print(x,i,j); cerr << endl;
+#else
+#define debugarr(x)
+#endif
+
+void _print(int t){cerr << t;}
+void _print(string t){cerr << t;}
+void _print(bool t){cerr << t;}
+void _print(char t){cerr << t;}
+void _print(long double t){cerr << t;}
+void _print(double t){cerr << t;}
+void _print(unsigned long long t){cerr << t;}
+template <class T> void _print(T arr[], int i, int j){cerr << "[ " ;for ( int k = i; k <= j; k++) cerr << arr[k] << ", " ;cerr << "]\n";}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p){cerr << "{";_print(p.first);cerr << ",";_print(p.second);cerr << "}";}
+template <class T> void _print(vector <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(set <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T> void _print(multiset <T> v){cerr << "[ ";for (T i : v){_print(i);cerr << " ";}cerr << "]";}
+template <class T, class V> void _print(map <T, V> v){cerr << "[ ";for (auto i : v){_print(i);cerr << " ";}cerr << "]";}
+
+int T, K, X;
+
+int countEmojis(int level)
+{
+	int ans = 0;
+	if(level <= K)
+		ans += level * (level + 1) / 2;
+	else
+	{
+		int total = K * (K + 1) / 2 + K * (K - 1) / 2;
+		int excess = level - K;
+		ans += total - ((K - excess) * (K - excess - 1) / 2);
+	}
+
+	return ans;
+}
+
+void solve()
+{
+ 	cin >> K >> X;
+ 	int lb = 1, ub = 2 * K - 1;
+
+ 	while(lb < (ub - 1))
+ 	{
+ 		int mid = lb + (ub - lb) / 2;
+ 		if(countEmojis(mid) < X)
+ 			lb = mid + 1;
+ 		else
+ 			ub = mid;
+ 	}
+
+ 	if(X <= countEmojis(lb))
+ 		cout << lb << '\n';
+ 	else
+ 		cout << ub << '\n';
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+        freopen("error.txt", "w", stderr);
+    #endif
+
+    cin >> T;
+    while (T--)
+        solve();
+
+    return 0;
+}
