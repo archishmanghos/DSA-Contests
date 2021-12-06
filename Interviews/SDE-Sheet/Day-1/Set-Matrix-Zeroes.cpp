10ee03673cc33b0d6257724c74e4923077caafcc
Archishman Ghosh <ag2324@it.jgec.ac.in>
2021-12-06 22:41:52 +0530

    Added solution to SDE Sheet Set Matrix Zeros

--- Interviews/SDE-Sheet/Day-1/Set-Matrix-Zeroes.cpp
+++ Interviews/SDE-Sheet/Day-1/Set-Matrix-Zeroes.cpp
@@ -1,5 +1,9 @@
 // brute, TC = O(N * M * (N + M)), SC = O(N*M)
 
+/*The brute force is fairly normal, we just take a vector
+and store the positions where the matrix has 0. We then iterate
+over the matrix again, and whenever we find a zero, we traverse the row and column and make the cells in them0 too.*/
+
 void setZeroes(vector<vector<int>> &matrix)
 {
     int n = matrix.size();
@@ -22,6 +26,10 @@ void setZeroes(vector<vector<int>> &matrix)
 
 // better brute, TC = O(N * M + N * M), SC = O(N + M)
 
+/*The brute method is bettered by taking 2 1-D arrays representing
+a dummy row and a dummy column. Whenever, we encounter 
+a 0 in the cell, we update the i-th cell of the column matrix with 0 and the j-th cell of the row matrix with 0. In the next traversal, we check if either of i and j of the row and column matrix are 0, if found, we make the cell 0*/
+
 void setZeroes(vector<vector<int>> &matrix)
 {
     int n = matrix.size();
@@ -39,6 +47,10 @@ void setZeroes(vector<vector<int>> &matrix)
 }
 
 // most optimal, TC = O(N * M + N * M), SC = O(1)
+
+/*This method is an optimisation of the previous method. We don't take the dummy arrays, 
+and instead make the 1st row and column the dummy matrices. 
+We assign a column variable to check if the lower column or the right row is making the [0,0] cell 0. In that case, we modify the bool variable, such that the other cells don't get affected. Take care to traverse the 2nd time backwards.*/
 
 void setZeroes(vector<vector<int>> &matrix)
 {
