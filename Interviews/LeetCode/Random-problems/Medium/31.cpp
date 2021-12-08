4d3aa2caa7141ee4bbac279723a5aef51a6e2ebe
Archishman Ghosh <ag2324@it.jgec.ac.in>
2021-12-08 18:53:36 +0530

    Add solution to 31 Next Permutation

---
+++ Interviews/LeetCode/Random-problems/Medium/31.cpp
@@ -0,0 +1,27 @@
+void nextPermutation(vector<int> &nums)
+{
+    int breakpoint1 = -1, breakpoint2, n = nums.size();
+    for(int i = n - 2; i >= 0; i--)
+    {
+        if(nums[i] < nums[i + 1])
+        {
+            breakpoint1 = i;
+            break;
+        }
+    }
+    if(breakpoint1 != -1)
+    {
+        for(int i = n - 1; i >= 0; i--)
+        {
+            if(nums[i] > nums[breakpoint1])
+            {
+                breakpoint2 = i;
+                break;
+            }
+        }
+
+        swap(nums[breakpoint1], nums[breakpoint2]);
+    }
+
+    reverse(nums.begin() + breakpoint1 + 1, nums.end());
+}
