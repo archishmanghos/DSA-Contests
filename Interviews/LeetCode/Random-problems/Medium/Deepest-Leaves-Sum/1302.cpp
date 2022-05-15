10be8a15165466a2868bd1c4f96f4bf7cdb86883
Archishman Ghosh <ag2324@it.jgec.ac.in>
2022-05-15 21:50:14 +0530

    Add solution to Leetcode 1302 Deepest Leaves Sum

--- Interviews/LeetCode/Random-problems/Medium/Deepest-Leaves-Sum/1302.cpp
+++ Interviews/LeetCode/Random-problems/Medium/Deepest-Leaves-Sum/1302.cpp
@@ -1,15 +1,15 @@
 int deepestLeavesSum(TreeNode* root) {
     queue<TreeNode *> q;
     q.push(root);
-    int ans = root->val;
-    
+    int ans = 0;
     while(!q.empty()){
         int sz = q.size();
-        int sum = 0;
+        ans = 0;
         for(int i = 1; i <= sz; i++){
             TreeNode *curNode = q.front();
             q.pop();
-            sum += curNode->val;
+            
+            ans += curNode->val;
             if(curNode->left){
                 q.push(curNode->left);
             }
@@ -17,7 +17,7 @@ int deepestLeavesSum(TreeNode* root) {
                 q.push(curNode->right);
             }
         }
-        ans = sum;
     }
+    
     return ans;
 }
--- Interviews/LeetCode/Random-problems/Medium/Deepest-Leaves-Sum/README.md
+++ Interviews/LeetCode/Random-problems/Medium/Deepest-Leaves-Sum/README.md
@@ -1,6 +1,16 @@
-My Solution: TC = `O(N)`, SC = `O(N)`
+[Link to problem on Leetcode](https://leetcode.com/problems/deepest-leaves-sum/)
+
+
+Optimal Solution: TC = `O(N)`, SC = `O(N)`
 
 > We perform a bfs and change the answer to the sum of Node values of the current level. <br>
 
 Runtime: `116 ms`, faster than `74.65%` <br>
 Memory Usage: `61.8 MB`, less than `43.93%`.
+
+
+<details><summary>Clean Code</summary>
+
+![](https://github.com/archishmanghos/code-images/blob/master/Leetcode/1302.png)
+
+</details>
