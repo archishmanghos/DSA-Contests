39c600cc86816806240e552a0c21827581d3e5a6
archishmanghos <ag2324@it.jgec.ac.in>
2022-05-30 20:44:48 +0000

    Add solution to GFG Subset Sums

--- Interviews/SDE-Sheet/Day-9-Recursion/Subset-Sums/code.cpp
+++ Interviews/SDE-Sheet/Day-9-Recursion/Subset-Sums/code.cpp
@@ -1,16 +1,3 @@
-vector<int> subsetSums(vector<int> arr, int N){
-    vector<int> ans;
-    for(int i = 0; i < (1 << N); i++){
-        int curSum = 0;
-        for(int j = 0; j < N; j++){
-            if(i & (1 << j)) curSum += arr[j];
-        }
-        ans.push_back(curSum);
-    }
-    
-    return ans;
-}
-
 void subsetSum(int index,int curSum,int &N,vector<int> &ans,vector<int> &arr){
     if(index == N){
         ans.push_back(curSum);
