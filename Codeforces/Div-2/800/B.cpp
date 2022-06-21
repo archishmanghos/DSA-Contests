bc351989b8d9a6d75cede31a1fd5579782851ff3
archishmanghos <ag2324@it.jgec.ac.in>
2022-06-21 11:58:40 +0530

    Add solution to B of Round 800

---
+++ Codeforces/Div-2/800/B.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+#ifndef ONLINE_JUDGE
+
+#include </home/archishmanghosh/archishmanghos/practice/archidebug.h>
+
+#else
+#define debug(x)
+#endif
+
+#define int long long
+using namespace std;
+
+const int mxN = 2e5 + 5;
+const int INF = 1e18 + 7;
+
+int T, N;
+string S;
+
+void solve() {
+	cin >> N >> S;
+	int ans = N;
+
+	for(int i = 1; i < N; i++) {
+		if(S[i] != S[i - 1]) ans += i;
+	}
+
+	cout << ans << '\n';
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+        freopen("error.txt", "w", stderr);
+    #endif
+
+    cin >> T;
+    while (T--)
+        solve();
+
+    return 0;
+}
