22eef0d90e4f933e0324d18ac007d766dd08aed2
archishmanghos <ag2324@it.jgec.ac.in>
2022-07-01 12:28:34 +0530

    Add solution to POSCS of Dbatu Starters 5.0

---
+++ Codechef/External-Rated/Dbatu-Starters-5/POSCS.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+#ifndef ONLINE_JUDGE
+
+#include </home/archishmanghosh/archishmanghos/practice/archidebug.h>
+
+#else
+#define debug(x)
+#endif
+
+#define int long long
+using namespace std;
+
+const int mxN = 2e5 + 5;
+const int INF = 1e18 + 7;
+
+int T, N, A[mxN];
+
+void solve() {
+    
+    cin >> N;
+    for(int i = 1; i <= N; i++) cin >> A[i];
+    sort(A + 1, A + N + 1);
+	int ans = A[2] * A[N - 1];
+	cout << ans << '\n';
+    
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+        freopen("error.txt", "w", stderr);
+    #endif
+
+    cin >> T;
+    while (T--)
+        solve();
+
+    return 0;
+}
