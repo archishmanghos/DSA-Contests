74e8e03ad1c0758f3475eb60dcb2c5ec2a8ed092
Archishman Ghosh <ag2324@it.jgec.ac.in>
2022-07-15 12:00:56 +0530

    Add solution to Leetcode 617 Merge Two Binary Trees

---
+++ Interviews/LeetCode/Topic-Wise/Trees/Binary-Trees/Easy/Merge-Two-Binary-Trees/617.cpp
@@ -0,0 +1,8 @@
+TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        if (!root1) return root2;
+        if (!root2) return root1;
+        TreeNode* root = new TreeNode(root1->val + root2->val);
+        root->left = mergeTrees(root1->left, root2->left);
+        root->right = mergeTrees(root1->right, root2->right);
+        return root;
+}
---
+++ Interviews/LeetCode/Topic-Wise/Trees/Binary-Trees/Easy/Merge-Two-Binary-Trees/README.md
@@ -0,0 +1,7 @@
+[Link to problem on Leetcode](https://leetcode.com/problems/merge-two-binary-trees/)
+
+
+Optimal Solution: TC = `O(N)`, SC = `O(1)`
+
+Runtime: `49 ms`, faster than `74.53%`<br>
+Memory Usage: `32.9 MB`, less than `36.41%`<br>
