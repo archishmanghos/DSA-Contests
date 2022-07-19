ff08b93103c2345e9f4bf4a11e312c76b75a8dde
Archishman Ghosh <ag2324@it.jgec.ac.in>
2022-07-19 17:44:55 +0530

    Add solution to Pascal's Triangle

--- Interviews/SDE-Sheet/Day-1-Arrays/Pascal-s-Triangle/118.cpp
+++ Interviews/SDE-Sheet/Day-1-Arrays/Pascal-s-Triangle/118.cpp
@@ -1,15 +1,14 @@
-vector<vector<int>> generate(int numRows){
+vector<vector<int>> generate(int numRows) {
     vector<vector<int>> ans(numRows);
-    
-    for(int i = 0; i < numRows; i++){
-        for(int j = 0; j <= i; j++){
-            if(i > 0 and j > 0){
-                ans[i].push_back((ans[i - 1].size() > j ? ans[i - 1][j - 1] + ans[i - 1][j] : 1));
-            }else{
+    for (int i = 0; i < numRows; i++) {
+        for (int j = 0; j <= i; j++) {
+            if (i > 0 and j > 0 and j < i) {
+                ans[i].push_back(ans[i - 1][j - 1] + ans[i - 1][j]);
+            } else {
                 ans[i].push_back(1);
             }
         }
     }
-    
+
     return ans;
 }
