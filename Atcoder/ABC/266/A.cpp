0dff932bf251727f54d49a79ccf05fdaf66d905c
Archishman Ghosh <ag2324@it.jgec.ac.in>
2022-08-28 23:45:01 +0530

    Add problem A of ABC 266

---
+++ Atcoder/ABC/266/A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#ifndef ONLINE_JUDGE
+#include <G:\DSA-Contests\archidebug.h>
+#else
+#define debug(x)
+#endif
+
+#define int long long
+using namespace std;
+
+int32_t main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	string s; cin >> s;
+	int n = s.size();
+	cout << s[n / 2];
+	return 0;
+}
