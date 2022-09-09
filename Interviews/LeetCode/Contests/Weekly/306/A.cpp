364cec7e0aee133c199d556c6ceef7b7fbe56db8
Archishman Ghosh <ag2324@it.jgec.ac.in>
2022-09-09 19:03:31 +0530

    Add solution to A of Weekly 306

---
+++ Interviews/LeetCode/Contests/Weekly/306/A.cpp
@@ -0,0 +1,18 @@
+vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
+    int n = grid.size();
+    vector<vector<int>> ans(n - 2, vector<int> (n - 2));
+    for (int i = 0; i < (n - 2); i++) {
+        for (int j = 0; j < (n - 2); j++) {
+            int maxx = 0;
+            for (int k = i; k < (i + 3); k++) {
+                for (int l = j; l < (j + 3); l++) {
+                    maxx = max(maxx, grid[k][l]);
+                }
+            }
+
+            ans[i][j] = maxx;
+        }
+    }
+
+    return ans;
+}
