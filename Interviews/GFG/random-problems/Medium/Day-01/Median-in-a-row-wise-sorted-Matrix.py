48c9c14254c92ddff26208ddfd5a0195959d49c2
Archishman Ghosh <ag2324@it.jgec.ac.in>
2022-11-17 19:00:46 +0530

    Add solution to GFG Count of Subarrays

--- Interviews/GFG/random-problems/Medium/Day-01/Median-in-a-row-wise-sorted-Matrix.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def median (self, matrix, R, C):
-        a = []
-    	for i in range(R):
-    	    for j in range(C):
-    	        a.append(matrix[i][j])
-    	a.sort()
-    	return a[(R * C) // 2]
