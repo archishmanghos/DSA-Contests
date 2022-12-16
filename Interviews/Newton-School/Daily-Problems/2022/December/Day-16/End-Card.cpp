565451a67c5f92e88a161655c4d9eb4c92d96ca2
Archishman Ghosh <ag2324@it.jgec.ac.in>
2022-12-16 09:31:23 +0530

    Add solution to NS End Card

---
+++ Interviews/Newton-School/Daily-Problems/2022/December/Day-16/End-Card.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int32_t main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, i;
+	cin >> n >> i;
+	cout << (n - i + 1);
+	return 0;
+}
---
+++ Interviews/Newton-School/Daily-Problems/2022/December/Day-16/README.md
@@ -0,0 +1,5 @@
+<details><summary>C++ code</summary>
+
+![](assets/20221216093104.png)
+
+</details>
