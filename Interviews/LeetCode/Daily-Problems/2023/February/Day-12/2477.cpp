1560fd0685a2fcc4eabac8b6dd618eb15ae838bd
Archishman Ghosh <ag2324@it.jgec.ac.in>
2023-02-12 20:23:41 +0530

    Add solution to LC 2477 Minimum Fuel Cost to Report to the Capital

---
+++ Interviews/LeetCode/Daily-Problems/2023/February/Day-12/2477.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+#ifndef ONLINE_JUDGE
+#include <G:\DSA-Contests\archidebug.h>
+#else
+#define debug(x)
+#endif
+
+#define int long long
+using namespace std;
+
+class Solution {
+public:
+	long long dfs(int node, int seats, vector<int> &vis, vector<vector<int>> &adj, long long &fuel) {
+		vis[node] = 1;
+		long long childs = 1;
+		for (int i = 0; i < adj[node].size(); i++) {
+			if (!vis[adj[node][i]]) {
+				childs += dfs(adj[node][i], seats, vis, adj, fuel);
+			}
+		}
+
+		if (node != 0) {
+			fuel += (childs + seats - 1) / seats;
+		}
+		return childs;
+	}
+	long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
+		int n = roads.size() + 1;
+		vector<vector<int>> adj(n);
+		for (int i = 0; i < n - 1; i++) {
+			int u = roads[i][0], v = roads[i][1];
+			adj[u].push_back(v);
+			adj[v].push_back(u);
+		}
+		vector<int> vis(n, 0);
+		long long fuel = 0;
+		long long childs = dfs(0, seats, vis, adj, fuel);
+		return fuel;
+	}
+};
