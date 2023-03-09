9954a49fafd548dc1b208f4ee4020d49b779fa00
Archishman Ghosh <ag2324@it.jgec.ac.in>
2023-03-09 19:04:59 +0530

    Add solution to problem D of Kickstart 2022 Round A

--- Companies/Google/Kickstart/2022/A/D.cpp
+++ Companies/Google/Kickstart/2022/A/D.cpp
@@ -1,57 +1,63 @@
 #include <bits/stdc++.h>
-
-// #ifndef ONLINE_JUDGE
-
-// #include <D:/archishmanghos/Practice/Online_Judges/archiDebug.h>
-
-// #else
-// #define debug(x)
-// #endif
-
 #define int long long
-using namespace std;
 
-const int mxN = 2e5 + 5;
-const int INF = 1e18 + 7;
+int cache[14][2][2][110][110];
+int dp(int idx, int start, int exceed, int sod, int rem, std::string &s) {
+	if (idx == s.size()) return (sod == 0 and rem == 1);
+	int &answer = cache[idx][start][exceed][sod][rem], end = (exceed ? 9 : (s[idx] - '0'));
+	if (answer != -1) return answer;
 
-int T, A, B;
-
-bool check(int X){
-	int pro = 1, sum = 0;
-	while(X){
-		pro *= (X % 10);
-		sum += (X % 10);
-		X /= 10;
-	}
-	return (pro % sum == 0);
-}
-
-void solve()
-{
-	cin >> A >> B;
-	int ans = 0;
-	for(int i = A; i <= B; i++){
-		ans += check(i);
+	answer = 0;
+	for (int i = 0; i <= end; i++) {
+		if (i <= sod) {
+			answer += dp(idx + 1, (start | (i > 0)), (exceed | (i < (s[idx] - '0'))), sod - i, (start or (i > 0) ? (rem / std::__gcd(rem, i)) : rem), s);
+		}
 	}
 
-	cout << ans << '\n';
+	return answer;
 }
 
-int32_t main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+std::string integerTostring (int x) {
+	if (x == 0) return "0";
+	std::string answer = "";
+	while (x) {
+		answer += (x % 10) + '0';
+		x /= 10;
+	}
 
-    // #ifndef ONLINE_JUDGE
-    //     freopen("error.txt", "w", stderr);
-    // #endif
+	reverse(answer.begin(), answer.end());
+	return answer;
+}
 
-    cin >> T;
-    for (int i = 1; i <= T; i++)
-    {
-        cout << "Case #" << i << ": ";
-        solve();
-    }
+void solve() {
+	int A, B; std::cin >> A >> B;
+	std::string _A = integerTostring(A - 1);
+	std::string _B = integerTostring(B);
 
-    return 0;
+	int n1 = _A.size(), n2 = _B.size();
+	int answer = 0;
+	for (int i = 1; i <= n2 * 9; i++) {
+		memset(cache, -1, sizeof(cache));
+		int ans = dp(0, 0, 0, i, i, _B);
+		answer += ans;
+	}
+	for (int i = 1; i <= n1 * 9; i++) {
+		memset(cache, -1, sizeof(cache));
+		int ans = dp(0, 0, 0, i, i, _A);
+		answer -= ans;
+	}
+
+	std::cout << answer << '\n';
+}
+
+int32_t main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+
+	int T; std::cin >> T;
+	for (int i = 1; i <= T; i++) {
+		std::cout << "Case #" << i << ": ";
+		solve();
+	}
+	return 0;
 }
