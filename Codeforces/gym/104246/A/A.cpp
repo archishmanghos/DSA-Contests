64a21b647185afcdc431a1ae949e18a83a1a73b4
Archishman Ghosh <ag2324@it.jgec.ac.in>
2023-03-22 10:44:55 +0530

    Add solution to problem A of CF Gym by RUET, Bangladesh

---
+++ Codeforces/gym/104246/A/A.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int32_t main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+
+	cout << "Programmer";
+	return 0;
+}
---
+++ Codeforces/gym/104246/A/README.md
@@ -0,0 +1,7 @@
+[A. AI vs Programmers](https://codeforces.com/gym/104246/problem/A)
+
+<details><summary>Solution</summary>
+
+![](../../../assets/104246A.png)
+
+</details>
