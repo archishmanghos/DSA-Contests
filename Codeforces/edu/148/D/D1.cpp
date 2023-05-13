ecbdfa2d69a2cc5a2ae217ed2176ad6b09c6d557
Archishman Ghosh <ag2324@it.jgec.ac.in>
2023-05-13 11:27:38 +0530

    Add solution to Problem D1 of Edu 148

---
+++ Codeforces/edu/148/D/D1.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int32_t main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+
+	int N, Q; cin >> N >> Q;
+	vector<int> A(N); for (int i = 0; i < N; i++) cin >> A[i];
+
+	sort(A.begin(), A.end());
+	while (Q--) {
+		int K; cin >> K;
+		vector<int> temp = A;
+		int n = N;
+		n -= (K % 2 != n % 2);
+
+		int j = K;
+		for (int i = 0; i < n; i++) temp[i] += j--;
+		sort(temp.begin(), temp.end());
+		for (int i = N - 1; i > 0; i--) {
+			int steps = min((temp[i] - temp[i - 1]) * (N - i) * 2, j);
+			temp[i] -= (steps + (N - i) * 2 - 1) / (2 * (N - i));
+			j -= steps;
+		}
+
+		sort(temp.begin(), temp.end());
+		int rem = temp[0] - (((j / 2) + N - 1) / N);
+		cout << rem << ' ';
+	}
+	return 0;
+}
---
+++ Codeforces/edu/148/D/README.md
@@ -0,0 +1,7 @@
+[D1. Red-Blue Operations (Easy Version)](https://codeforces.com/contest/1832/problem/D1)
+
+<details><summary>Solution</summary>
+
+![](../../../assets/1832D1.png)
+
+</details>
