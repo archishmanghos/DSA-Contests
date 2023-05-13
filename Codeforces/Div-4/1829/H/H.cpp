485ce1f12510b518ba61086b1f1e8b647c46bb93
Archishman Ghosh <ag2324@it.jgec.ac.in>
2023-05-13 13:36:31 +0530

    Add solution to Problem H of CF Round 871

---
+++ Codeforces/Div-4/1829/H/H.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+int N, K, A[200200], cache[200200][64][2];
+
+int dp(int idx, int val, int taken, int MOD = 1e9 + 7) {
+	if (idx == N) {
+		if (!taken) return 0;
+		return __builtin_popcount(val) == K;
+	}
+
+	if (cache[idx][val][taken] != -1) return cache[idx][val][taken];
+	return cache[idx][val][taken] = (dp(idx + 1, val, taken) % MOD + dp(idx + 1, (!taken ? A[idx] : (val & A[idx])), 1) % MOD) % MOD;;
+}
+
+void solve() {
+	cin >> N >> K;
+	for (int i = 0; i < N; i++) cin >> A[i];
+	memset(cache, -1, sizeof(cache));
+	cout << dp(0, 0, 0) << '\n';
+}
+
+int32_t main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+
+	int T; std::cin >> T;
+	while (T--)
+		solve();
+	return 0;
+}
---
+++ Codeforces/Div-4/1829/H/README.md
@@ -0,0 +1,7 @@
+[H. Don't Blame Me](https://codeforces.com/contest/1829/problem/H)
+
+<details><summary>Solution</summary>
+
+![](../../../assets/1829H.png)
+
+</details>
