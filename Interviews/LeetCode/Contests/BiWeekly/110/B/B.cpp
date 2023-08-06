8ae5f019f78cd6a2438ac4cce6c0d9298733d413
Archishman <ag2324@it.jgec.ac.in>
2023-08-06 13:32:59 +0530

    Add solution to problem D of Biweekly 110

--- Interviews/LeetCode/Contests/BiWeekly/110/B/B.cpp
+++ Interviews/LeetCode/Contests/BiWeekly/110/B/B.cpp
@@ -13,16 +13,20 @@ class Solution {
 public:
 	ListNode* insertGreatestCommonDivisors(ListNode* head) {
 		vector<int> values;
+		ListNode *temp = head;
 		while (head->next) {
 			int a = head->val, b = head->next->val;
 			values.push_back(__gcd(a, b));
+			head = head->next;
 		}
 
+		head = temp;
 		if (values.size() == 0) return head;
 
-		int i = 0l;
+
+		int i = 0;
 		ListNode *answer = head;
-		while (head->next) {
+		while (head and head->next) {
 			ListNode *temp = head->next;
 			head->next = new ListNode(values[i++]);
 			head = head->next;
