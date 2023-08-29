c429a67373bfcbb45dabf6b6364bcc294ffae397
Archishman Ghosh <ag2324@it.jgec.ac.in>
2023-08-29 22:06:13 +0530

    Add solution to LC 2483. Minimum Penalty for a Shop

---
+++ Interviews/Placement-Preparation/Day31-29August2023/codes/1.java
@@ -0,0 +1,25 @@
+public class LC2483 {
+    public int bestClosingTime(String customers) {
+        int n = customers.length();
+        int[] suffix = new int[n];
+
+        suffix[n - 1] = customers.charAt(n - 1) == 'Y' ? 1 : 0;
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] + (customers.charAt(i) == 'Y' ? 1 : 0);
+        }
+
+        int min_penalty = (int) 1e9, answer = -1, closed_shops = 0;
+        for (int i = 0; i < n; i++) {
+            int penalty = closed_shops + suffix[i];
+            if (penalty < min_penalty) {
+                min_penalty = penalty;
+                answer = i;
+            }
+
+            closed_shops += customers.charAt(i) == 'N' ? 1 : 0;
+        }
+
+        if (closed_shops < min_penalty) return n;
+        return answer;
+    }
+}
