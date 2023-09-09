aabf0c7c4b2a1e916ce0991fade8a4845fd00bc7
Archishman <ag2324@it.jgec.ac.in>
2023-09-09 16:14:02 +0530

    Add solution to LC 377 377. Combination Sum IV

---
+++ Interviews/Placement-Preparation/Day42-09September2023/codes/1.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class LC377 {
+    public int dp(int[] nums, int target, int[] cache) {
+        if (target == 0) return 1;
+        if (cache[target] != -1) return cache[target];
+
+        int answer = 0;
+        for (int num : nums) {
+            if (num <= target) {
+                answer += dp(nums, target - num, cache);
+            }
+        }
+
+        return cache[target] = answer;
+    }
+
+    public int combinationSum4(int[] nums, int target) {
+        int[] cache = new int[target + 1];
+        Arrays.fill(cache, -1);
+        return dp(nums, target, cache);
+    }
+}
