319ef9d9d6d97b45c33eba09746a3398655ef904
Archishman <ag2324@it.jgec.ac.in>
2023-09-29 07:10:32 +0530

    Add solution to GFG Left View of Binary Tree

---
+++ Interviews/Placement-Preparation/Day62-29September2023/codes/1.java
@@ -0,0 +1,37 @@
+package GFG;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+
+public class BT_Left_View {
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int item) {
+            data = item;
+            left = right = null;
+        }
+    }
+
+    ArrayList<Integer> leftView(Node root) {
+        Deque<Node> q = new ArrayDeque<>();
+        ArrayList<Integer> answer = new ArrayList<>();
+        if (root != null) q.offerLast(root);
+
+        while (!q.isEmpty()) {
+            int sz = q.size();
+            for (int i = 1; i <= sz; i++) {
+                Node node = q.pollFirst();
+                assert node != null;
+                if (i == 1) answer.add(node.data);
+
+                if (node.left != null) q.offerLast(node.left);
+                if (node.right != null) q.offerLast(node.right);
+            }
+        }
+
+        return answer;
+    }
+}
