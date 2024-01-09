ee6a9be4e902cb1eb4bd27690bff2101924680aa
Archishman <ag2324@it.jgec.ac.in>
2024-01-09 21:43:15 +0530

    Add solution to LC 872. Leaf-Similar Trees

---
+++ Interviews/Placement-Preparation/Day164-09January2023/codes/1.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC872 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() { }
+
+        TreeNode(int val) { this.val = val; }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    List<Integer> leafVal = new ArrayList<>();
+    int idx = 0;
+
+    private boolean checkSymmetry(TreeNode root, boolean check) {
+        if (root.left == null && root.right == null) {
+            if (check) {
+                if (idx == leafVal.size() || leafVal.get(idx) != root.val) return false;
+                ++idx;
+                return true;
+            }
+            leafVal.add(root.val);
+            return true;
+        }
+
+        if (root.left != null && !checkSymmetry(root.left, check)) return false;
+        return root.right == null || checkSymmetry(root.right, check);
+    }
+
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        checkSymmetry(root1, false);
+        return checkSymmetry(root2, true) && idx == leafVal.size();
+    }
+}
